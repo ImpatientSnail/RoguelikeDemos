45439db019f713a7a1a23e6d4ac476589d79caa6
nickkato <63608817+nickkato@users.noreply.github.com>
2023-06-25 22:25:42 -0700

    Unplayable

---
+++ Assets/PlayerFiniteStateMachine/Scripts/Player/Data/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newPlayerData", menuName ="Data/Player Data/Base data")]
+public class PlayerData : ScriptableObject
+{
+   [Header("Move State")]
+   public float movementVelocity = 10f;
+
+   [Header("Jump State")]
+   public float jumpVelocity = 15f;
+
+   [Header("Check Variables")]
+   public float groundCheckRadius = 0.3f;
+   public LayerMask whatIsGround;
+} 
